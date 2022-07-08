71c5d18d9e4ba305c14283e4ad0c74525f24c322
Hardik Patel <92606493+shpweb@users.noreply.github.com>
2022-07-08 15:19:14 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            region = "ap-south-1"
+        }
+    }
+}
+
 resource "aws_instance" "myec2" {
     ami             = "ami-08df646e18b182346"
     instance_type   = "t2.micro" 
