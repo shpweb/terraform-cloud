5d2c1e03cb1a98ef4819c50614685ce45632782b
Hardik Patel <92606493+shpweb@users.noreply.github.com>
2022-07-08 23:23:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -13,4 +13,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
     ami             = "ami-08df646e18b182346"
     instance_type   = "t2.micro" 
+    tags            =   {
+        Name        = ec2-tfcloud
+    }
 }
